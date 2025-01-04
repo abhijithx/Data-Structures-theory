06f149a027c3360986611c0769825ca1f6c99b6b
abhijithx <abhijithsudhakaran123@gmail.com>
2025-01-04 12:41:56 +0530

    .

---
+++ BubbleSort.java
@@ -0,0 +1,9 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7, 9, 34, 78, 99, 100};
+        System.out.println(nums.length);
+        
+    
+}
+    }
